45910a76f411231625242199a546bab370312441
duongnd <ndduong070523@gmail.com>
2025-05-06 02:09:04 +0700

    fix: fix deploy err

--- src/main/java/org/example/hrm/config/DatabaseConfig.java
+++ src/main/java/org/example/hrm/config/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package org.example.hrm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
--- src/main/java/org/example/hrm/security/SpringSecurityAuditorAware.java
+++ src/main/java/org/example/hrm/security/SpringSecurityAuditorAware.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component
+@Component("springSecurityAuditorAware")
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
