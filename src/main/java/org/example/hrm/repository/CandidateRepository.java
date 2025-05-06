43c8c90af9df41f76f9501b61ee29ad319661dac
duongnd <ndduong070523@gmail.com>
2025-05-07 00:17:18 +0700

    update: hasInterview field in search candidate

--- src/main/java/org/example/hrm/repository/CandidateRepository.java
+++ src/main/java/org/example/hrm/repository/CandidateRepository.java
@@ -13,15 +13,30 @@ import java.util.Set;
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
     @Query(value = """
-        SELECT * FROM candidate c
-        WHERE (:keyword IS NULL OR LOWER(c.full_name) LIKE LOWER(CONCAT('%', :keyword, '%')))
-        """,
+    SELECT DISTINCT c.* FROM candidate c
+    LEFT JOIN candidate_interview ci ON c.id = ci.candidate_id
+    WHERE (:keyword IS NULL OR LOWER(c.full_name) LIKE LOWER(CONCAT('%', :keyword, '%')))
+      AND (
+        :hasInterview IS NULL
+        OR (:hasInterview = true AND ci.interview_id IS NOT NULL)
+        OR (:hasInterview = false AND ci.interview_id IS NULL)
+      )
+    """,
             countQuery = """
-        SELECT COUNT(*) FROM candidate c
-        WHERE (:keyword IS NULL OR LOWER(c.full_name) LIKE LOWER(CONCAT('%', :keyword, '%')))
-        """,
+    SELECT COUNT(DISTINCT c.id) FROM candidate c
+    LEFT JOIN candidate_interview ci ON c.id = ci.candidate_id
+    WHERE (:keyword IS NULL OR LOWER(c.full_name) LIKE LOWER(CONCAT('%', :keyword, '%')))
+      AND (
+        :hasInterview IS NULL
+        OR (:hasInterview = true AND ci.interview_id IS NOT NULL)
+        OR (:hasInterview = false AND ci.interview_id IS NULL)
+      )
+    """,
             nativeQuery = true)
-    Page<Candidate> search(@Param("keyword") String keyword, Pageable pageable);
+    Page<Candidate> search(@Param("keyword") String keyword,
+                           @Param("hasInterview") Boolean hasInterview,
+                           Pageable pageable);
+
 
 
     Candidate findByEmail(String email);
